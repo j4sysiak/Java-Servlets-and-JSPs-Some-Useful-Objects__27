05a63eff70e2c0f871db7e084208ef64bdf08aa9
j4sysiak <j4sysiak@gmail.com>
2019-03-10 16:17:31 +0100

    1

---
+++ Java-Servlets-and-JSPs-Some-Useful-Objects__27/src/demo/Session.java
@@ -0,0 +1,93 @@
+package demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Session
+ */
+public class Session extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Session() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+			HttpSession session = request.getSession();
+			
+//			create Cart object by session object
+			Cart cart = (Cart)session.getAttribute("cart");
+			
+			if(cart==null){
+				cart = new Cart();
+			}
+			
+			cart.setTotalItems(7);
+			
+//			insert atribute of Cart by session object
+			session.setAttribute("cart", cart);
+			
+			getServletContext().getRequestDispatcher("/showcart.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
